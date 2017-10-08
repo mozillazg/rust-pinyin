dee38f658d9d655fa16ae7be339ec890e6a4ff98
mozillazg <mozillazg101@gmail.com>
2017-10-08 10:58:47 +0800

    add more tests
    add test_non_chinese_pinyin and test_non_chinese_lazy_pinyin

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,13 +3,15 @@ extern crate pinyin;
 struct TestCase {
     args: pinyin::Args,
     result: Vec<Vec<String>>,
+    lazy_result: Vec<String>,
 }
 
 impl TestCase {
-    pub fn new(args: pinyin::Args, result: Vec<Vec<String>>) -> TestCase {
+    pub fn new(args: pinyin::Args, result: Vec<Vec<String>>, lazy_result: Vec<String>) -> TestCase {
         TestCase {
             args: args,
             result: result,
+            lazy_result: lazy_result,
         }
     }
 }
@@ -26,6 +28,7 @@ fn test_pinyin() {
                 vec!["guo".to_string()],
                 vec!["ren".to_string()],
             ],
+            vec!["zhong".to_string(), "guo".to_string(), "ren".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -37,6 +40,7 @@ fn test_pinyin() {
                 vec!["guo".to_string()],
                 vec!["ren".to_string()],
             ],
+            vec!["zhong".to_string(), "guo".to_string(), "ren".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -48,6 +52,7 @@ fn test_pinyin() {
                 vec!["guó".to_string()],
                 vec!["rén".to_string()],
             ],
+            vec!["zhōng".to_string(), "guó".to_string(), "rén".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -59,6 +64,7 @@ fn test_pinyin() {
                 vec!["guo2".to_string()],
                 vec!["re2n".to_string()],
             ],
+            vec!["zho1ng".to_string(), "guo2".to_string(), "re2n".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -70,6 +76,7 @@ fn test_pinyin() {
                 vec!["g".to_string()],
                 vec!["r".to_string()],
             ],
+            vec!["zh".to_string(), "g".to_string(), "r".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -81,6 +88,7 @@ fn test_pinyin() {
                 vec!["g".to_string()],
                 vec!["r".to_string()],
             ],
+            vec!["z".to_string(), "g".to_string(), "r".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -92,6 +100,7 @@ fn test_pinyin() {
                 vec!["uo".to_string()],
                 vec!["en".to_string()],
             ],
+            vec!["ong".to_string(), "uo".to_string(), "en".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -103,6 +112,7 @@ fn test_pinyin() {
                 vec!["uó".to_string()],
                 vec!["én".to_string()],
             ],
+            vec!["ōng".to_string(), "uó".to_string(), "én".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -114,6 +124,7 @@ fn test_pinyin() {
                 vec!["uo2".to_string()],
                 vec!["e2n".to_string()],
             ],
+            vec!["o1ng".to_string(), "uo2".to_string(), "e2n".to_string()],
         ),
         TestCase::new(
             pinyin::Args {
@@ -125,19 +136,44 @@ fn test_pinyin() {
                 vec!["guo".to_string()],
                 vec!["ren".to_string()],
             ],
+            vec!["zhong".to_string(), "guo".to_string(), "ren".to_string()],
         ),
     ];
     for data in &test_data {
-        assert_eq!(&data.result, &pinyin::pinyin(hans, &data.args));
-
-        let mut lazy_result: Vec<String> = Vec::new();
-        for ret in &data.result {
-            if !ret.is_empty() {
-                lazy_result.push(ret[0].to_string());
-            }
-        }
-        assert_eq!(lazy_result, pinyin::lazy_pinyin(hans, &data.args));
+        assert_eq!(data.result, pinyin::pinyin(hans, &data.args));
+        assert_eq!(data.lazy_result, pinyin::lazy_pinyin(hans, &data.args));
     }
+}
+
+#[test]
+fn test_non_chinese_pinyin() {
+    let hans = "中国人abc你好";
+    let expect = vec![
+        vec!["zhong".to_string()],
+        vec!["guo".to_string()],
+        vec!["ren".to_string()],
+        vec![],
+        vec![],
+        vec![],
+        vec!["ni".to_string()],
+        vec!["hao".to_string()],
+    ];
+    let result = pinyin::pinyin(hans, &pinyin::Args::new());
+    assert_eq!(expect, result);
+}
+
+#[test]
+fn test_non_chinese_lazy_pinyin() {
+    let hans = "中国人abc你好";
+    let expect = vec![
+        "zhong".to_string(),
+        "guo".to_string(),
+        "ren".to_string(),
+        "ni".to_string(),
+        "hao".to_string(),
+    ];
+    let result = pinyin::lazy_pinyin(hans, &pinyin::Args::new());
+    assert_eq!(expect, result);
 }
 
 #[test]
