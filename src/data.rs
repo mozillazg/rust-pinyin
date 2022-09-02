dbac31764d7eb1d4ddd07c352ab73ad3d4d24846
Kevin Li <kevinli020508@gmail.com>
2022-09-01 21:43:06 -0400

    Generate phrase table

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::unreadable_literal)]
 
 use crate::{CharBlock, PinyinData};
+use std::collections::HashMap;
 
 pub(crate) static PINYIN_DATA: &[PinyinData] =
     include!(concat!(env!("OUT_DIR"), "/pinyin_data.rs"));
@@ -10,3 +11,6 @@ pub(crate) static HETERONYM_TABLE: &[&[u16]] =
     include!(concat!(env!("OUT_DIR"), "/heteronym_table.rs"));
 
 pub(crate) static CHAR_BLOCKS: &[CharBlock] = include!(concat!(env!("OUT_DIR"), "/char_blocks.rs"));
+
+pub(crate) static PHRASE_TABLE: &[(&'static str, &[&[usize]])] =
+    include!(concat!(env!("OUT_DIR"), "/phrase_table.rs"));
